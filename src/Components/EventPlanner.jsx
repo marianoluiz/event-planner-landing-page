5eafaf4382654c3cd118f65ef5e5c9048bf0bdc7
Richa Arora <richaaeri9988@gmail.com>
2024-03-21 16:19:29 +0530

    initial commit

---
+++ src/Components/EventPlanner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './EventPlanner.css'; // Import CSS file for styling
+
+const EventPlanner = () => {
+    return (
+        <div className="event-planner-container">
+            <header>
+                <h1>Welcome to Event Planner</h1>
+            </header>
+        </div>
+    );
+};
+
+export default EventPlanner;
